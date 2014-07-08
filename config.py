86eff2e7f3115826ecda1b429fd0acf972eed98f
Thomas Martin <twoflowers@users.noreply.github.com>
2014-07-08 13:16:21 -0500

    Update config.py
    removed useless application peices

--- config.py
+++ config.py
@@ -3,12 +3,5 @@ CODERO_API_URL = 'https://apis.codero.com/cloud/v1/'
 CODERO_API_CODELET = "NEC9TVgKWsMqnwvOVonEWUSI9uM="
 CODERO_API_BILLING_TYPE = "hourly"
 
-OPENID_PROVIDERS = [
-    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
-    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
-    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
-    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
-    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
-
 SRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
